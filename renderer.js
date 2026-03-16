afe65a17c0c7d6cfe03fc22c2bc7996db169c42e
Surya <nitrodead22@gmail.com>
2026-03-16 15:21:48 +0530

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -1,30 +1,47 @@
-const { exec } = require("child_process");
+const { exec } = require("child_process")
 
-function scan() {
+function log(msg){
+let logs=document.getElementById("logs")
+logs.innerHTML+="<br>"+msg
+logs.scrollTop=logs.scrollHeight
+}
 
-let target = document.getElementById("target").value;
+function searchTarget(){
 
-document.getElementById("webview").src = "http://" + target;
+let target=document.getElementById("target").value
 
+document.getElementById("browser").src="http://"+target
 
-// DNS lookup
+log("[+] Target loaded")
 
-exec(`python3 python/dnslookup.py ${target}`, (err, stdout) => {
-document.getElementById("ip").innerText = stdout;
-});
+exec(`python3 python/dnslookup.py ${target}`,(e,o)=>{
+document.getElementById("ip").innerText=o
+log("[+] IP detected")
+})
 
+exec(`python3 python/portscan.py ${target}`,(e,o)=>{
+document.getElementById("ports").innerText=o
+log("[+] Ports scanned")
+})
 
-// Port scan
+exec(`python3 python/subdomains.py ${target}`,(e,o)=>{
+document.getElementById("subdomains").innerText=o
+log("[+] Subdomains found")
+})
 
-exec(`python3 python/portscan.py ${target}`, (err, stdout) => {
-document.getElementById("ports").innerText = stdout;
-});
+exec(`python3 python/crawler.py http://${target}`,(e,o)=>{
+document.getElementById("pages").innerText=o
+log("[+] Pages discovered")
+})
 
+exec(`python3 python/dirscan.py ${target}`,(e,o)=>{
+document.getElementById("dirs").innerText=o
+log("[+] Hidden directories found")
+})
 
-// Page crawler
-
-exec(`python3 python/crawler.py http://${target}`, (err, stdout) => {
-document.getElementById("pages").innerText = stdout;
-});
+exec(`python3 python/techdetect.py ${target}`,(e,o)=>{
+document.getElementById("tech").innerText=o
+log("[+] Technology detected")
+})
 
 }
