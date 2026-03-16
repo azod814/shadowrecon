d752061017b8b3560788d1ca27126252f8b9e66a
Surya <nitrodead22@gmail.com>
2026-03-16 13:14:10 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+const {app, BrowserWindow} = require("electron")
+
+function createWindow(){
+
+const win = new BrowserWindow({
+width:1400,
+height:900,
+webPreferences:{
+nodeIntegration:true
+}
+})
+
+win.loadFile("index.html")
+
+}
+
+app.whenReady().then(createWindow)
